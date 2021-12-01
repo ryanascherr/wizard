4e469e5e4f9d45877d50a341e1dd4b12acb0f2fe
ryanascherr <ryanascherr@gmail.com>
2021-12-01 14:59:47 -0500

    Added transition between questions and logic for single select and mutliple choice.

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
+let currentQuestion = 1;
+
 $(".answer").click(function() {
-    if ($(this).hasClass("selected")) {
-        $(this).removeClass("selected");
-        return;
+    if (currentQuestion === 1) {
+        if ($(this).hasClass("selected")) {
+            $(this).removeClass("selected");
+            return;
+        } else {
+            $(this).addClass("selected");
+        }
+    } else {
+        if ($(this).hasClass("selected")) {
+            return;
+        } else {
+            $(".answer").removeClass("selected");
+            $(this).addClass("selected");
+        }
     }
-    $(this).addClass("selected");
 });
 
 $(".btn").click(function() {
     if ($(".answer").hasClass("selected")) {
         console.log("Nice choice!");
+        handleQuestionChange();
         return;
     }
     console.log("Gotta select something!");
 });
+
+function handleQuestionChange() {
+    console.log("Ya");
+    $(".one").slideUp(1000);
+    $(".box-box").slideToggle(1000);
+}
