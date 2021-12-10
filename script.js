e94db3c9b8815aa0c7f26ec306406573c52a97dc
ryanascherr <ryanascherr@gmail.com>
2021-12-10 11:16:17 -0500

    Simplied code. Reduced lines of code significantly.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let currentQuestion = 1;
 let currentAnswer = '';
+let gameSelection = [];
 
 let games = [
     {name: 'Playstation Casual First-Person Shooter', console: 'playstation', genre: 'fps', intensity: 'casual'},
@@ -22,24 +23,7 @@ let games = [
     {name: 'Switch Intense Puzzler', console: 'switch', genre: 'puzzle', intensity: 'intense'}
 ];
 
-let gameSelection = [];
-
 $(".answer").click(function() {
-    // if (currentQuestion === 1) {
-    //     if ($(this).hasClass("selected")) {
-    //         $(this).removeClass("selected");
-    //         return;
-    //     } else {
-    //         $(this).addClass("selected");
-    //     }
-    // } else {
-    //     if ($(this).hasClass("selected")) {
-    //         return;
-    //     } else {
-    //         $(".answer").removeClass("selected");
-    //         $(this).addClass("selected");
-    //     }
-    // }
     currentAnswer = $(this).data("answer");
     if ($(this).hasClass("selected")) {
         return;
@@ -54,56 +38,34 @@ $(".btn").click(function() {
         $(".answer").removeClass("selected");
         if (currentQuestion === 1) {
             gameSelection = games.filter(game => game.console == currentAnswer);
-            handleQuestionOneChange();
-            return;
         }
         if (currentQuestion === 2) {
             gameSelection = gameSelection.filter(game => game.genre == currentAnswer);
-            handleQuestionTwoChange();
-            return;
         }
         if (currentQuestion === 3) {
             gameSelection = gameSelection.filter(game => game.intensity == currentAnswer);
-            handleQuestionThreeChange();
-            return;
         }
+        handleQuestionChange();
+        currentQuestion++;
         return;
     }
     console.log("Gotta select something!");
 });
 
-function handleQuestionOneChange() {
-    $(".one").slideUp(1000);
-    $(".box-box-one").slideToggle(1000);
-    currentQuestion++;
-    $(".progress-box-one").css('background-position', 'left');
-    $(".progress-line-one").css('background-position', 'left');
+function handleQuestionChange() {
+    let currentBox = $(".question-box")
+    [currentQuestion-1];
+    $(currentBox).slideUp(1000);
+    let currentBoxBox = $(".test")[currentQuestion-1];
+    $(currentBoxBox).slideToggle(1000);
+    let progressLine = $(".progress-line")[currentQuestion-1];
+    $(progressLine).css('background-position', 'left');
+    let progressBox = $(".progress-box")[currentQuestion];
+    if (currentQuestion === 3) {
+        $(".game").text(gameSelection[0].name);
+        $(".btn").addClass("hidden");
+   }
     setTimeout(function(){
-        $(".progress-box-two").css('background', '#02833E');
-   }, 1000);
-}
-
-function handleQuestionTwoChange() {
-    $(".two").slideUp(1000);
-    $(".box-box-two").slideToggle(1000);
-    currentQuestion++;
-    $(".progress-box-two").css('background-position', 'left');
-    $(".progress-line-two").css('background-position', 'left');
-    setTimeout(function(){
-        $(".progress-box-three").css('background', '#02833E');
-   }, 1000);
-}
-
-function handleQuestionThreeChange() {
-    console.log("three");
-    $(".btn").addClass("hidden");
-    $(".game").html(gameSelection[0].name);
-    $(".three").slideUp(1000);
-    $(".box-box-three").slideToggle(1000);
-    currentQuestion++;
-    $(".progress-box-three").css('background-position', 'left');
-    $(".progress-line-three").css('background-position', 'left');
-    setTimeout(function(){
-        $(".progress-box-four").css('background', '#02833E');
+        $(progressBox).css('background', '#02833E');
    }, 1000);
 }
